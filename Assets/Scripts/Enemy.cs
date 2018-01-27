0e9a206e23875ae0130b68885c68d49a1a6fdd2f
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-27 15:26:52 -0500

    Added pirate enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CheckpointChecker.respawnPlayer(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyPirate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPirate : Enemy {
+
+    public float moveDistance = 5;//how far from the center he can move
+    public float moveSpeed = 3;//how fast he moves
+
+    private Vector3 startPos;
+    private float moveDirection = 1;//which direction he is moving
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (Mathf.Abs(transform.position.x - startPos.x) >= moveDistance)
+        {
+            moveDirection *= -1;
+        }
+        rb.velocity = new Vector3(moveDirection * moveSpeed, rb.velocity.y);
+	}
+
+    
+}
