9f1eaac8f3233f435b5544bbafaca0ad424eaaf9
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-27 11:24:10 -0500

    Fixed #13 Camera Controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject playerObject;
+
+    private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+        offset = gameObject.transform.position - playerObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            playerObject.transform.position + offset,
+            10);
+	}
+}
