d0a4d340e50c1b0d115f8e5271e3e8453bbec9c6
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-27 11:08:26 -0500

    Level manager added, added two demo scenes

---
+++ Assets/LevelGoal.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGoal : MonoBehaviour {
+
+    /// <summary>
+    /// The level that should be loaded after this ends.
+    /// Leave it 0 to automatically go to the next one
+    /// </summary>
+    public int nextLevelNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            if (nextLevelNumber == 0)
+            {
+                LevelManager.LoadNextLevel();
+            }
+            else
+            {
+                LevelManager.LoadLevel(nextLevelNumber);
+            }
+        }
+    }
+}
