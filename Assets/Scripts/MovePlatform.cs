559cc1c2bdc010c4f7f4e3900d0a7d2ca21a3d9c
Brett <lordbap@gmail.com>
2018-01-27 18:44:21 -0500

    Level features in

---
+++ Assets/Scripts/MovePlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlatform : MonoBehaviour {
+
+	Vector3 startEnd;
+	Vector3 finishEnd;
+	Vector3 myPos;
+	float step = 1f;
+
+	// Use this for initialization
+	void Start () {
+		myPos = this.transform.position;
+		startEnd = new Vector3 (myPos.x - step, myPos.y, myPos.z);
+		finishEnd = new Vector3 (myPos.x + step, myPos.y, myPos.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(startEnd, finishEnd, Mathf.PingPong(Time.time, step));
+	}
+}
---
+++ Assets/Scripts/RisePlatform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RisePlatform : MonoBehaviour {
+
+	Vector3 startEnd;
+	Vector3 finishEnd;
+	Vector3 myPos;
+	float step = 1f;
+
+	// Use this for initialization
+	void Start () {
+		myPos = this.transform.position;
+		startEnd = new Vector3 (myPos.x, myPos.y - step, myPos.z);
+		finishEnd = new Vector3 (myPos.x, myPos.y + step, myPos.z);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.Lerp(startEnd, finishEnd, Mathf.PingPong(Time.time, step));
+	}
+}
