dab8b883e034d0d01c55db73e2602436365c9d7a
Brett <lordbap@gmail.com>
2018-01-28 01:28:54 -0500

    Menu Stuff

---
+++ Assets/Scripts/ConfigControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConfigControl : MonoBehaviour {
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			string myJob = this.name;
+			if (SceneManager.GetSceneByName("ControlSettings").isLoaded)
+			{
+				SceneManager.UnloadSceneAsync("ControlSettings");
+			}
+
+			if (myJob == "MainMenu")
+				LoadMenu ();
+
+		}
+	}
+	void LoadMenu(){
+		SceneManager.LoadScene ("MainMenu",LoadSceneMode.Additive);
+	}
+}
---
+++ Assets/Scripts/MenuControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour {
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			string myJob = this.name;
+			if (SceneManager.GetSceneByName("MainMenu").isLoaded)
+			{
+				SceneManager.UnloadSceneAsync("MainMenu");
+			}
+
+			if (myJob == "PlayLevel")
+				PlayLevel ();
+			else if (myJob == "RandomLevel")
+				RandomLevel ();
+			else if (myJob == "CreditsScreen")
+				CreditScreen ();
+			else  if (myJob == "ControlSettings")
+				ControlSettings ();
+			else  if (myJob == "Exit")
+				Application.Quit();
+		}
+	}
+
+	void PlayLevel(){
+			SceneManager.LoadScene ("Level1",LoadSceneMode.Additive);
+	}
+
+	void RandomLevel(){
+			SceneManager.LoadScene ("RandomLevel",LoadSceneMode.Additive);
+	}
+
+	void CreditScreen(){
+			SceneManager.LoadScene ("CreditsScreen",LoadSceneMode.Additive);
+	}
+
+	void ControlSettings(){
+			SceneManager.LoadScene ("ControlSettings",LoadSceneMode.Additive);
+	}
+
+
+}
