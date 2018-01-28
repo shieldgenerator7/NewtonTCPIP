7c6f9f97a4cd8354d13d5630c17514dde80730cf
shieldgenerator7 <shieldgenerator7@yahoo.com>
2018-01-27 21:29:55 -0500

    Added DDOS enemy

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,39 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float moveSpeed = 7.0f;
-    public float jumpSpeed = 5.0f;
-    public float jumpDuration = 0.1f;//how long the jump can be 
-    public int maxJumpCount = 2;//how many jumps per ground he can do (2 = "double jump")
-
-    private float lastGroundTime;//the last time he was on the ground
-    [SerializeField]
-    private int jumpCount = 0;
-    private bool jumpStarted = false;
+    public float moveSpeed = 7.0f;   
 
     private Rigidbody rb;
+    private JumpAbility ja;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ja = GetComponent<JumpAbility>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void jump(float amount)
     {
-
+        ja.jump(amount);
     }
 
-    private void OnCollisionStay(Collision collision)
+    public void cancelJump()
     {
-        if (collision.contacts[0].point.y <= transform.position.y)
-        {
-            lastGroundTime = Time.time;
-            jumpCount = 0;
-            jumpStarted = false;
-        }
+        ja.cancelJump();
     }
 
     /// <summary>
@@ -48,34 +35,6 @@ public class PlayerController : MonoBehaviour
     public void moveHorizontally(float direction)
     {
         rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
-    }
-    /// <summary>
-    /// Makes the character jump
-    /// </summary>
-    /// <param name="jumpAmount"></param>
-    public void jump(float jumpAmount)
-    {
-        if (!jumpStarted)
-        {
-            jumpStarted = true;
-            jumpCount++;
-            lastGroundTime = Time.time;
-        }
-        if (Time.time <= lastGroundTime + jumpDuration) {
-            jumpAmount = Mathf.Max(0, jumpAmount);
-            rb.velocity = new Vector2(rb.velocity.x, jumpAmount * jumpSpeed);
-        }
-    }
-    public void cancelJump()
-    {
-        if (rb.velocity.y > 0)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
-            if (jumpCount < maxJumpCount)
-            {
-                jumpStarted = false;
-            }
-        }
     }
 
     /// <summary>
